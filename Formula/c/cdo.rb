dae258ce061d33b3af526b16d8eed89be65efd5e
botantony <antonsm21@gmail.com>
2026-05-18 16:33:59 +0200

    cdo 2.6.1
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/c/cdo.rb
+++ Formula/c/cdo.rb
@@ -1,8 +1,8 @@
 class Cdo < Formula
   desc "Climate Data Operators"
   homepage "https://code.mpimet.mpg.de/projects/cdo"
-  url "https://code.mpimet.mpg.de/attachments/download/30182/cdo-2.6.0.tar.gz"
-  sha256 "752d5cda6fa3fdb8a04dcea16af5918e5f9f54657d9a5d2e35ae34f5755c31d8"
+  url "https://code.mpimet.mpg.de/attachments/download/30210/cdo-2.6.1.tar.gz"
+  sha256 "ccf5f3bd5800f703c031bb5b10ae0cd3feac34d8eba7956661ff1ba6deb5985f"
   license "BSD-3-Clause"
 
   livecheck do
@@ -43,6 +43,8 @@ class Cdo < Formula
   end
 
   def install
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version if OS.mac?
+
     args = %W[
       --disable-openmp
       --with-eccodes=#{Formula["eccodes"].opt_prefix}
